33deeaf87bfbfc6252e6675c4e5ffda4b8c8ec41
ostapenko-kostia <fodnichanel@gmail.com>
2021-12-04 15:09:52 +0200

    1.1

--- js/script.js
+++ js/script.js
@@ -8,40 +8,35 @@ function change(value) {
 	if (value == "+") {
 		what = "+";
 		console.log(value);
-		return(what);
 	}
 	if (value == "-") {
 		what = "-";
 		console.log(value);
-		return(what);
 	}
 	if (value == "x") {
 		what = "*";
 		console.log(value);
-		return(what);
 	}
 	if (value == "/") {
 		what = "/";
 		console.log(value);
-		return(what);
 	}
 	b = document.getElementById("output").innerHTML += value;
-
 	if (value == "=") {
 		console.log(what);
-		// if (what == "+") {
-		// 	c = a + b;
-		// }
-		// if (what == "-") {
-		// 	c = a - b;
-		// }
-		// if (what == "*") {
-		// 	c = a * b;
-		// }
-		// if (what == "/") {
-		// 	c = a / b;
-		// }
-		// document.getElementById("output").innerHTML = c;
+		if (what == "+") {
+			c = a + b;
+		}
+		if (what == "-") {
+			c = a - b;
+		}
+		if (what == "*") {
+			c = a * b;
+		}
+		if (what == "/") {
+			c = a / b;
+		}
+		document.getElementById("output").innerHTML = c;
 	}
 	if (value == "ac") {
 		document.getElementById("output").innerHTML = "0";
