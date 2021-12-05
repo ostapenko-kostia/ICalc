def3ffe827cf499bcd7d108497666ee588945d28
ostapenko-kostia <fodnichanel@gmail.com>
2021-12-05 14:05:07 +0200

    File version is 1.4

--- js/script.js
+++ js/script.js
@@ -1,46 +1,71 @@
-function change(value) {
-	var a, b;
-	var what;
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+const out = document.querySelector(".calc-display p");
+
+function clear_all() {
+	a = '';
+	b = '';
+	c = '';
+	let what = '';
+	sign = '';
+	finish = false;
+	out.textContent = 0;
+}
+
+function count() {
 	var c;
-	document.getElementById("output").innerHTML += value;
-	if (!value == "+" && !value == "x" && !value == "/" && !value == "-") {
-		a = document.getElementById("output").innerHTML;
+	if (sign == '+') {
+		c = parseInt(a) + parseInt(b);
+		finish = true;
 	}
-	if (value == "+") {
-		what = "+";
-		console.log(value);
+	if (sign == '-') {
+		c = parseInt(a) - parseInt(b);
+		finish = true;
 	}
-	if (value == "-") {
-		what = "-";
-		console.log(value);
+	if (sign == 'X') {
+		c = parseInt(a) * parseInt(b);
+		finish = true;
 	}
-	if (value == "x") {
-		what = "*";
-		console.log(value);
+	if (sign == '/') {
+		c = parseInt(a) / parseInt(b);
+		finish = true;
 	}
-	if (value == "/") {
-		what = "/";
-		console.log(value);
-	}
-	b = document.getElementById("output").innerHTML;
-	if (value == "=") {
-		console.log(what);
-		if (what == "+") {
-			c = a + b;
+	document.getElementById('output').textContent = c;
+	console.log("Result: " + c + "\nType: " + typeof(c) + "\nFinished: " + finish + "\nOutput: " + out.textContent);
+}
+
+document.querySelector(".ac").onclick = clear_all;
+
+document.querySelector(".calc-buttons").onclick = (event) => {
+	if(!event.target.classList.contains('calc-btn')) return;
+	if(event.target.classList.contains('ac')) return;
+
+	out.textContent = '';
+	const key = event.target.textContent;
+
+	if (digit.includes(key)) {
+		if (b === '' && sign === '') {
+			a += key;
+			out.textContent = a;
+		} else {
+			b += key;
+			out.textContent = b;
 		}
-		if (what == "-") {
-			c = a - b;
-		}
-		if (what == "*") {
-			c = a * b;
-		}
-		if (what == "/") {
-			c = a / b;
-		}
-		document.getElementById("output").innerHTML = c;
+		console.log(a, b, sign);
+		return;
 	}
-	if (value == "ac") {
-		document.getElementById("output").innerHTML = "0";
+
+	if (action.includes(key)) {
+		sign = key;
+		out.textContent = sign;
+		console.log(a, b, sign);
+		return;
 	}
 }
 
+document.querySelector(".equal").onclick = count;
